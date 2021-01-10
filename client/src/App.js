ddc2caf9974f936ee22a4deea3fce89f320c6525
Shradhdha Parsana <shradhdha03@gmail.com>
2021-01-10 02:48:15 -0500

    added image def

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Books from './Books';
 function App() {
   return (
     <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-danger">
         <div className="collapse navbar-collapse justify-content-md-center">
           <ul className="navbar-nav">
             <li className="nav-item active">
